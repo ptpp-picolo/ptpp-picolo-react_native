d042eba9e6803d7892e1af7f347340c68c96d7e6
hchauptfeld <haley.hauptfeld@gmail.com>
2020-07-19 18:47:01 -0400

    got the name list working

--- App.js
+++ App.js
@@ -2,27 +2,57 @@ import React, {Component} from 'react';
 import { StyleSheet, View, Text, Button, Alert, TextInput } from 'react-native';
 
 var playerNames = [] ;
+   const ruleDeck = [
+     'Take 3 sips if you say someone elses name',
+     'Take a sip if you are wearing blue',
+     'Reverse buffalo is in effect',
+     'Switch shirts with the person to your left',
+     'Kill your drink'
+   ];
+const MIN_SIP = 2
+const MAX_SIP = 6
 
 export default class MainActivity extends Component {
 
   constructor(props) {
        super(props)
        this.state = {
-         Holder: ''
-       }
+         text: ''
+       };
      }
+
+
+     getRule=(ruleDeck)=>{
+       const index = Math.floor(Math.random() * ruleDeck.length)
+       Alert.alert(ruleDeck[index])
+     }
+
+     getRandomPlayer=(playerNames)=>{
+       const index = Math.floor(Math.random() * playerNames.length)
+       return playerNames[index]
+     }
+
+    getRandomSip=()=>{
+       return Math.floor(Math.random() * (MAX_SIP - MIN_SIP + 1)) + MIN_SIP
+     }
+
+
+
+
 
   AddItemsToArray=()=>{
 
       //Adding Items To Array.
-      playerNames.push( this.state.Holder.toString() );
+      playerNames.push(this.state.text.toString());
 
       // Showing the complete Array on Screen Using Alert.
       Alert.alert("List of Current Players: ", playerNames.toString());
 
+      this.textInput.clear()
   }
 
  render() {
+
 
    return (
       <View style={styles.MainContainer}>
@@ -32,16 +62,20 @@ export default class MainActivity extends Component {
           </Text>
 
           <TextInput
-              placeholder="Enter a player name here"
-              onChangeText={TextInputValue => this.setState({ Holder : TextInputValue }) }
+              ref={input => { this.textInput = input }}
+              placeholder = "Enter a player name"
+              onChangeText={(text) => this.setState({ text }) }
+              value={this.state.TextInputValue}
               style={{textAlign: 'center', marginBottom: 6, height: 45}}
           />
+
+          <>{playerNames}</>
 
 
           <Button color='#778899' title="Click here to add a player to the game!" onPress={this.AddItemsToArray} />
 
 
-          <Button color='#778899' title="Click here to begin the game!"/>
+          <Button color='#778899' title="Click here to begin the game!" onPress={this.getRule}/>
 
       </View>
 
